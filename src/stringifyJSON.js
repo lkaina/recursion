db6fb98242b9562fb8de4bd1684f4c50271c74f4
Leighton Kaina <leighton.kaina@gmail.com>
2013-09-11 20:44:38 -0700

    Finished stringifyJSON.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,35 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+	var toType = function(obj) {
+  		return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
+	};
+
+	var getString = function(newObj){
+		var oType = toType(newObj);
+		
+		if (oType === 'object') {
+			var result = [];
+			for (var prop in newObj){
+				if (!(toType(newObj[prop]) === 'function' || toType(newObj[prop]) === 'undefined')){ result.push('"'+prop+'":'+stringifyJSON(newObj[prop])); }
+			}
+			return '{'+result+'}';
+		}
+		else if(oType === 'array'){
+			var result = [];
+			for (var i = 0; i < newObj.length; i++){
+				 result.push(stringifyJSON(newObj[i]));
+			}
+			return '['+result+']';
+		}
+	  	
+		else if(oType === 'string'){
+			return '"'+newObj+'"';
+		}
+
+		else return String(newObj);
+	};
+
+	return getString(obj);
+
 };
